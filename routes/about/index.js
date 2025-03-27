d38c4a3e80a087884db6857eb75d8771c2b9d541
Arkar Myat <arkardev00@gmail.com>
2025-03-27 00:08:56 -0400

    no vibe coding

--- routes/about/index.js
+++
@@ -1,7 +0,0 @@
-// Create WebSocket connection.
-const socket = new WebSocket("ws://localhost:3000/__hmr");
-
-// Listen for messages
-socket.addEventListener("message", (event) => {
-  console.log("", event.data);
-});
